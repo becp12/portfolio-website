f8f5cc05c36c0108d462002a3edda82aa984f92a
becp12 <rebecca-preece@outlook.com>
2022-09-25 16:31:27 -0700

    Adding project data file to pull project card info

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+        "thumbnailPath": "https://i.imgur.com/5plyU87.png",
+        "title": "Blackjack",
+        "projectLink": "https://becp12.github.io/Blackjack/",
+        "githubLink": "https://github.com/becp12/Blackjack",
+    },
+    {
+        "thumbnailPath": "https://i.imgur.com/2C9wlnj.png",
+        "title": "In The Loop",
+        "projectLink": "https://in-the-loop-crochet.herokuapp.com/projects",
+        "githubLink": "https://github.com/becp12/in-the-loop",
+    },
+    {
+        "thumbnailPath": "https://i.imgur.com/2uroDuX.png",
+        "title": "Simply Recipes",
+        "projectLink": "https://simply-recipes-621.herokuapp.com/",
+        "githubLink": "https://github.com/becp12/recipeapp",
+    },
+    {
+        "thumbnailPath": "https://i.imgur.com/bIFJfHA.png",
+        "title": "ConsoleLog",
+        "projectLink": "https://consolelogapp.herokuapp.com/",
+        "githubLink": "https://github.com/becp12/ConsoleLog",
+    },
+]
