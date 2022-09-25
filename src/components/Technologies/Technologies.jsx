2be099b15c956642e01844b78a4ca2a096557d67
becp12 <rebecca-preece@outlook.com>
2022-09-25 16:34:08 -0700

    Adding technologies/skills jsx and CSS

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -3,7 +3,50 @@ import './Technologies.css';
 export default function Technologies() {
   return (
     <div className="technologies">
-      <h1>Technologies/Languages</h1>
+      <h1>Skills</h1>
+      <p>I've worked with a range of technologies.</p>
+      <div className="logo-container">
+        {/* ROW 1 */}
+        {/* HTML */}
+        <img src="https://i.imgur.com/EdYAHNF.png" alt="" />
+        {/* CSS */}
+        <img src="https://i.imgur.com/TeFgZyQ.png" alt="" />
+        {/* bootstap */}
+        <img src="https://i.imgur.com/McgitMu.png" alt="" />
+        {/* JavaScript */}
+        <img src="https://i.imgur.com/leFdkwL.png" alt="" />
+        {/* React */}
+        <img src="https://i.imgur.com/RGavFiS.png" alt="" />
+        {/* Python */}
+        <img src="https://i.imgur.com/CqEO0w6.png" alt="" />
+        {/* ROW 2 */}
+        <h1>HTML</h1>
+        <h1>CSS</h1>
+        <h1>Bootstrap</h1>
+        <h1>JavaScript</h1>
+        <h1>React</h1>
+        <h1>Python</h1>
+        {/* ROW 3 */}
+        {/* Node.js */}
+        <img src="https://i.imgur.com/aZlVAxg.png" alt="" />
+        {/* express */}
+        <img src="https://i.imgur.com/VOgOLTM.png" alt="" />
+        {/* mongoose */}
+        <img src="https://i.imgur.com/TiDUvti.png" alt="" />
+        {/* mongoDB */}
+        <img src="https://i.imgur.com/bHffPog.png" alt="" />
+        {/* django */}
+        <img src="https://i.imgur.com/FLDpBt3.png" alt="" />
+        {/* postgresql */}
+        <img src="https://i.imgur.com/QrIH5i5.png" alt="" />
+        {/* ROW 4 */}
+        <h1>Node.js</h1>
+        <h1>Express</h1>
+        <h1>Mongoose</h1>
+        <h1>MongoDB</h1>
+        <h1>Django</h1>
+        <h1>PostgreSQL</h1>
+      </div>
     </div>
   );
 }
