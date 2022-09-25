cb586bfb05866ce393eb5d8236c1801fa9353c17
becp12 <rebecca-preece@outlook.com>
2022-09-24 19:51:04 -0700

    Adding dynamic year to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,12 @@
 import './Footer.css';
 
 export default function Footer() {
+  const d = new Date();
+  const currentYear = d.getFullYear();
+
   return (
     <div className="footer">
-        <p>&copy; 2022 Rebecca Preece. All Rights Reserved</p>
+        <p>&copy; {currentYear > 2022 ? `2022 - ${currentYear}` : '2022'} Rebecca Preece. All Rights Reserved</p>
     </div>
   );
 }
